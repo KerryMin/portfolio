87565400b2d570d707c27f74a63349a5f29cb314
Kerry Minott <kerrym175@gmail.com>
2023-09-19 18:18:29 -0400

    removed zelda
    removed zelda

--- src/configs/launchpad.ts
+++ src/configs/launchpad.ts
@@ -7,12 +7,6 @@ const launchpadApps: LaunchpadData[] = [
     title: "What 2 Watch",
     img: "img/icons/launchpad/film.png",
     link: "https://github.com/KerryMin/what2watch"
-  },
-  {
-    id: "zelda",
-    title: "Webpage for Zelda",
-    img: "img/icons/launchpad/zelda.png",
-    link: "https://zelda.zxh.io/"
   }
 ];
 
